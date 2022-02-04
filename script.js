2263bc681130a61c7b68a55ab2dc3292a94260b8
Егор <95488963+AbzMijon@users.noreply.github.com>
2022-02-04 12:44:17 +0300

    main_work

---
+++ script.js
@@ -0,0 +1,18 @@
+let artem = {
+    name:'artem',
+    age:19
+}
+function prob(i) {
+ vlad = new Boolean ([1])
+if (vlad == false) {
+    document.write('false')
+}
+else {
+    for (let i = 0; i < 10; i++) {
+        document.write(i);
+        
+    }
+}
+}
+prob(1);
+prob(0)
