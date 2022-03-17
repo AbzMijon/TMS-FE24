b08174d9139ae2e3961bf136df8fd32799bb32a0
Егор <95488963+AbzMijon@users.noreply.github.com>
2022-03-17 18:08:51 +0300

    swiperJS---

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-let artem = {
-    name:'artem',
-    age:19
-}
-function prob(i) {
- vlad = new Boolean ([1])
-if (vlad == false) {
-    document.write('false')
-}
-else {
-    for (let i = 0; i < 10; i++) {
-        document.write(i);
-        
-    }
-}
-}
-prob(1);
-prob(0)
+new Swiper('.swiper')
