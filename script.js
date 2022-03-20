5bda9e49a187f0a3f5713497cd5ca21632fc2c03
Егор <95488963+AbzMijon@users.noreply.github.com>
2022-03-20 15:45:15 +0300

    swiperJS

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const swiper = new Swiper('.swiper', {
     //pagination
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
+    },
+    keyboard: {
+        OnlyInViewport:true,
     }
 });
 
